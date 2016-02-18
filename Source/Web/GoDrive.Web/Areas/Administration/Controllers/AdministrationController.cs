0eeec8bbc8a7323c47788b6a3d0090bb4905226d
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-18 12:26:34 +0200

    Added dropdown with simple usernames in CreateOrganization view.

--- Source/Web/GoDrive.Web/Areas/Administration/Controllers/AdministrationController.cs
+++ Source/Web/GoDrive.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -1,10 +1,12 @@
 ﻿namespace GoDrive.Web.Areas.Administration.Controllers
 {
     using System.Web.Mvc;
+    using System.Web.Mvc.Expressions;
 
     using GoDrive.Common;
     using GoDrive.Web.Controllers;
-
+    using ViewModels.Organizations;
+    using System.Collections.Generic;
     [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
     public class AdministrationController : BaseController
     {
@@ -13,9 +15,41 @@
             return this.View();
         }
 
+        [HttpGet]
         public ActionResult CreateOrganization()
         {
+            this.BindUsers();
             return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateOrganization(CreateOganizationViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                this.BindUsers();
+                return this.View(model);
+            }
+
+            // TODO: Create the organization
+            return this.RedirectToAction<AdministrationController>(c => c.Index());
+        }
+
+        private void BindUsers()
+        {
+            var usersList = new List<SelectListItem>()
+            {
+                new SelectListItem() { Text = "pesho", Value = "1" },
+                new SelectListItem() { Text = "Gosho", Value = "1" },
+                new SelectListItem() { Text = "IIvan", Value = "1" },
+                new SelectListItem() { Text = "sta", Value = "1" },
+                new SelectListItem() { Text = "daw", Value = "1" },
+                new SelectListItem() { Text = "daw", Value = "1" },
+                new SelectListItem() { Text = "daw", Value = "1" },
+                new SelectListItem() { Text = "daw", Value = "1" }
+            };
+
+            this.ViewData["users"] = usersList;
         }
     }
 }
---
+++ Source/Web/GoDrive.Web/Areas/Administration/ViewModels/Organizations/CreateOganizationViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace GoDrive.Web.Areas.Administration.ViewModels.Organizations
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    public class CreateOganizationViewModel
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [MaxLength(3000)]
+        [Display(Name = "Aditional Information")]
+        public string AboutInfo { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+    }
+}
