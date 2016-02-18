4c86f8a32d800a701b090b5b4d1263de72d677cc
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-18 15:43:46 +0200

    Added OrganizationsController and View with all organizations

---
+++ Source/Web/GoDrive.Web/Controllers/OrganizationsController.cs
@@ -0,0 +1,28 @@
+﻿namespace GoDrive.Web.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+    using Infrastructure.Mapping;
+    using Services.Data.Contracts;
+    using ViewModels.Organizations;
+
+    public class OrganizationsController : Controller
+    {
+        private IOrganizationsService organizations;
+
+        public OrganizationsController(IOrganizationsService organizations)
+        {
+            this.organizations = organizations;
+        }
+
+        public ActionResult Index()
+        {
+            var organizations = this.organizations
+                .GetALl()
+                .To<OrganizationListViewModel>()
+                .ToList();
+
+            return this.View(organizations);
+        }
+    }
+}
