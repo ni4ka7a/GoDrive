27345945b37c462190e6fa52661416ea7f05616e
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-18 10:57:24 +0200

    Empty ManageUSers and ManageOrganizations Controllers added.

---
+++ Source/Web/GoDrive.Web/Areas/Administration/Controllers/ManageOrganizationsController.cs
@@ -0,0 +1,12 @@
+﻿namespace GoDrive.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ManageOrganizationsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
