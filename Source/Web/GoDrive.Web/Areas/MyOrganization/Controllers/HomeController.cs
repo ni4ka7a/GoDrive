320571efe47b4ea0966f6902ef2cb1492fcea957
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-20 11:18:14 +0200

    Created MyOrganization area and fixed the route config.

---
+++ Source/Web/GoDrive.Web/Areas/MyOrganization/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace GoDrive.Web.Areas.MyOrganization.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Source/Web/GoDrive.Web/Areas/MyOrganization/MyOrganizationAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿namespace GoDrive.Web.Areas.MyOrganization
+{
+    using System.Web.Mvc;
+
+    public class MyOrganizationAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "MyOrganization";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "MyOrganization_default",
+                "MyOrganization/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "GoDrive.Web.Areas.MyOrganization.Controllers" }
+            );
+        }
+    }
+}
