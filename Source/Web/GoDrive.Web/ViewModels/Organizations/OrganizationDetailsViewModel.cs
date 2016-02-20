a2ff9916856b9ee768c6c649bd867c621ec1e5ea
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-20 14:01:14 +0200

    Organization Details page added.

---
+++ Source/Web/GoDrive.Web/ViewModels/Organizations/OrganizationDetailsViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace GoDrive.Web.ViewModels.Organizations
+{
+    using System;
+    using System.Linq;
+    using AutoMapper;
+    using Data.Models;
+    using Infrastructure.Mapping;
+
+    public class OrganizationDetailsViewModel : IMapFrom<Organization>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string AboutInfo { get; set; }
+
+        public int UsersCount { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<Organization, OrganizationDetailsViewModel>()
+               .ForMember(x => x.UsersCount, opt => opt.MapFrom(x => x.Students.Where(c => c.IsDeleted == false).Count()));
+        }
+    }
+}
