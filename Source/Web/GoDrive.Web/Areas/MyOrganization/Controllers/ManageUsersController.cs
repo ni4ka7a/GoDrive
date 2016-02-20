5537f6d0dea31fa14ed906f158d1cdd9ad0ec912
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-20 12:47:42 +0200

    Added MyOrganization index page and MyOrganization/ManageUsers page in MyOrganization area.

---
+++ Source/Web/GoDrive.Web/Areas/MyOrganization/Controllers/ManageUsersController.cs
@@ -0,0 +1,20 @@
+﻿namespace GoDrive.Web.Areas.MyOrganization.Controllers
+{
+    using System.Web.Mvc;
+    using Services.Data.Contracts;
+
+    public class ManageUsersController : Controller
+    {
+        private IUsersService users;
+
+        public ManageUsersController(IUsersService users)
+        {
+            this.users = users;
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
