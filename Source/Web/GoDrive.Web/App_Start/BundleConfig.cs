f956516db61bec790eb89ff4ee48f43fa1d95a3d
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-21 00:52:18 +0200

    ManageRequest controller and view added in MyOrganization area.

--- Source/Web/GoDrive.Web/App_Start/BundleConfig.cs
+++ Source/Web/GoDrive.Web/App_Start/BundleConfig.cs
@@ -13,7 +13,9 @@
 
         private static void RegisterScripts(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery.unobtrusive-ajax.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
             bundles.Add(new ScriptBundle("~/bundles/materialize").Include("~/Scripts/materialize.min.js"));
---
+++ Source/Web/GoDrive.Web/Areas/MyOrganization/Controllers/ManageRequestsController.cs
@@ -0,0 +1,31 @@
+﻿namespace GoDrive.Web.Areas.MyOrganization.Controllers
+{
+    using System.Web.Mvc;
+    using Services.Data.Contracts;
+    using Web.Controllers;
+
+    public class ManageRequestsController : BaseController
+    {
+        private IJoinOrganizationService joinOrganizationRequests;
+
+        public ManageRequestsController(IJoinOrganizationService joinOrganizationRequests)
+        {
+            this.joinOrganizationRequests = joinOrganizationRequests;
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        public ActionResult GetUnProceedRequest()
+        {
+            return this.PartialView("_UsersRequestPartial");
+        }
+
+        public ActionResult GetProceedRequest()
+        {
+            return this.PartialView("_UsersRequestPartial");
+        }
+    }
+}
