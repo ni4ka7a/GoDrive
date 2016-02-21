e20728090b78336f9a78c393c2dc65edca14b039
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-21 11:39:29 +0200

    Upload car image logic implemented.

---
+++ Source/Services/GoDrive.Services.Data/CarImagesService.cs
@@ -0,0 +1,23 @@
+﻿namespace GoDrive.Services.Data
+{
+    using System;
+    using Contracts;
+    using GoDrive.Data.Common;
+    using GoDrive.Data.Models;
+
+    public class CarImagesService : ICarImagesService
+    {
+        private IDbRepository<CarImage> carImages;
+
+        public CarImagesService(IDbRepository<CarImage> carImages)
+        {
+            this.carImages = carImages;
+        }
+
+        public void Add(CarImage image)
+        {
+            this.carImages.Add(image);
+            this.carImages.Save();
+        }
+    }
+}
---
+++ Source/Web/GoDrive.Web/Areas/MyOrganization/Controllers/ManageCarImagesController.cs
@@ -0,0 +1,77 @@
+﻿namespace GoDrive.Web.Areas.MyOrganization.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using Common;
+    using Data.Models;
+    using Microsoft.AspNet.Identity;
+    using Services.Data.Contracts;
+    using Web.Controllers;
+
+    public class ManageCarImagesController : BaseController
+    {
+        // TODO: replace this with carsImagesService
+        private IOrganizationImagesService organizationImages;
+        private IOrganizationsService organizations;
+        private ICarImagesService carImages;
+
+        public ManageCarImagesController(
+            IOrganizationsService organizations,
+            IOrganizationImagesService organizationImages,
+            ICarImagesService carImages)
+        {
+            this.organizations = organizations;
+            this.organizationImages = organizationImages;
+            this.carImages = carImages;
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadCarImage(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                var extension = file.FileName.Substring(file.FileName.LastIndexOf("."));
+
+                if (!this.organizationImages.ValidateFileExtention(extension))
+                {
+                    this.TempData["error"] = $"The Image Should be {GlobalConstants.JpgFileExtension} or {GlobalConstants.PngFileExtension}."
+                    return this.RedirectToAction("Index");
+                }
+
+                var currentUserId = this.User.Identity.GetUserId();
+                var organization = this.organizations
+                    .GetALl()
+                    .Where(o => o.UserId == currentUserId)
+                    .FirstOrDefault();
+
+                var imageName = Guid.NewGuid();
+                var savePath = $"/Images/{organization.Name}/{imageName}{extension}";
+                var saveDirPathMapped = System.IO.Path.GetDirectoryName(this.Server.MapPath(savePath));
+                if (!System.IO.Directory.Exists(saveDirPathMapped))
+                {
+                    System.IO.Directory.CreateDirectory(saveDirPathMapped);
+                }
+
+                file.SaveAs(this.Server.MapPath(savePath));
+
+                var image = new CarImage()
+                {
+                    Name = imageName.ToString(),
+                    Url = savePath,
+                    Organization = organization
+                };
+
+                this.carImages.Add(image);
+            }
+
+            return this.RedirectToAction("Index");
+        }
+    }
+}
