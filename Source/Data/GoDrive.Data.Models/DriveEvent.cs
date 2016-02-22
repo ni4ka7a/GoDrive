7f52173f8e831c021e22cc514374974abc938bdf
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-22 09:52:53 +0200

    DriveEvent model added.

---
+++ Source/Data/GoDrive.Data.Models/DriveEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace GoDrive.Data.Models
+{
+    using System;
+    using Common.Models;
+
+    public class DriveEvent : BaseModel<int>
+    {
+        public string Title { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        public bool IsAllDay { get; set; }
+
+        public string UserID { get; set; }
+
+        public User User { get; set; }
+    }
+}
