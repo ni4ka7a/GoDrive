836de37e2ea77dfce67a2ed5529f9134cc362d5a
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-22 09:46:38 +0200

    Bundles for kendo added.

--- Source/Web/GoDrive.Web/App_Start/BundleConfig.cs
+++ Source/Web/GoDrive.Web/App_Start/BundleConfig.cs
@@ -19,12 +19,20 @@
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
             bundles.Add(new ScriptBundle("~/bundles/materialize").Include("~/Scripts/materialize.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                      "~/Scripts/KendoUI/kendo.all.min.js",
+                      "~/Scripts/KendoUI/kendo.aspnetmvc.min.js"));
         }
 
         private static void RegisterStyles(BundleCollection bundles)
         {
             // bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/materialize.min.css", "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo").Include(
+                     "~/Content/KendoUI/kendo.common.min.css",
+                     "~/Content/KendoUI/kendo.default.min.css"));
         }
     }
 }
