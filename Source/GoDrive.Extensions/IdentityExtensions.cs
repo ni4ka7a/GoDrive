ac069ec39382627b466c35bc033a313907cc15b7
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-22 16:48:33 +0200

    Extended User.Identiy and GetOrganizationId method added.

---
+++ Source/GoDrive.Extensions/IdentityExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace GoDrive.Extensions
+{
+    using System.Security.Claims;
+    using System.Security.Principal;
+
+    public static class IdentityExtensions
+    {
+        public static string GetOrganizationId(this IIdentity identity)
+        {
+            var claim = ((ClaimsIdentity)identity).FindFirst("OrganizationId");
+
+            return (claim != null) ? claim.Value : string.Empty;
+        }
+    }
+}
