e86b0fb5a73f3f5d805c7cb88edb7802f848d87e
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-22 15:29:45 +0200

    Joined Organizations controller and view added.

---
+++ Source/Web/GoDrive.Web/Controllers/JoindedOrganizationController.cs
@@ -0,0 +1,45 @@
+﻿namespace GoDrive.Web.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+    using Services.Data.Contracts;
+    using Microsoft.AspNet.Identity;
+    using Infrastructure.Mapping;
+    using ViewModels.JoinedOrganization;
+    [Authorize]
+    public class JoindedOrganizationController : BaseController
+    {
+        private IOrganizationsService organizations;
+        private IUsersService users;
+
+        public JoindedOrganizationController(IUsersService users)
+        {
+            this.users = users;
+        }
+
+
+        public ActionResult Index()
+        {
+            var currentUserId = this.User.Identity.GetUserId();
+
+            var userHaveJoinedOrganization = this.users
+                .GetAll()
+                .Where(u => u.Id == currentUserId)
+                .Any(u => u.JoinedOrganizationId != null);
+
+            if (!userHaveJoinedOrganization)
+            {
+                return this.View();
+            }
+
+            var organization = this.users
+                .GetAll()
+                .Where(u => u.Id == currentUserId)
+                .Select(x => x.JoinedOrganization)
+                .To<JoinedOrganizationViewModel>()
+                .FirstOrDefault();
+
+            return this.View(organization);
+        }
+    }
+}
---
+++ Source/Web/GoDrive.Web/ViewModels/JoinedOrganization/JoinedOrganizationViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GoDrive.Web.ViewModels.JoinedOrganization
+{
+    using Data.Models;
+    using Infrastructure.Mapping;
+
+    public class JoinedOrganizationViewModel : IMapFrom<Organization>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string AboutInfo { get; set; }
+    }
+}
