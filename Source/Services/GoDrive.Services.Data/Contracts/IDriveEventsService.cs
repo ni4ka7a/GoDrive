b2e7b59ae4a02d8ebd8d7a57755fd05dad38d01e
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-22 10:00:13 +0200

    DriveEvent service added.

---
+++ Source/Services/GoDrive.Services.Data/Contracts/IDriveEventsService.cs
@@ -0,0 +1,16 @@
+﻿namespace GoDrive.Services.Data.Contracts
+{
+    using GoDrive.Data.Models;
+    using System.Linq;
+
+    public interface IDriveEventsService
+    {
+        IQueryable<DriveEvent> GetAll();
+
+        void Update(DriveEvent driveEvent);
+
+        void Create(DriveEvent driveEvent);
+
+        void Delete(DriveEvent driveEvent);
+    }
+}
---
+++ Source/Services/GoDrive.Services.Data/DriveEventsService.cs
@@ -0,0 +1,42 @@
+﻿namespace GoDrive.Services.Data
+{
+    using System;
+    using System.Linq;
+    using Contracts;
+    using GoDrive.Data.Common;
+    using GoDrive.Data.Models;
+
+    public class DriveEventsService : IDriveEventsService
+    {
+        private IDbRepository<DriveEvent> driveEvents;
+
+        public DriveEventsService(IDbRepository<DriveEvent> driveEvents)
+        {
+            this.driveEvents = driveEvents;
+        }
+
+        public void Create(DriveEvent driveEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(DriveEvent driveEvent)
+        {
+            if (driveEvent != null)
+            {
+                this.driveEvents.Delete(driveEvent);
+                this.driveEvents.Save();
+            }
+        }
+
+        public IQueryable<DriveEvent> GetAll()
+        {
+            return this.driveEvents.All();
+        }
+
+        public void Update(DriveEvent driveEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
