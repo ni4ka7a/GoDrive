1cca5c34e1ac080048436b2c3098715b48e07dd1
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-02-22 18:30:30 +0200

    THe real car images shown in Organization details view.

--- Source/Web/GoDrive.Web/ViewModels/Organizations/OrganizationDetailsViewModel.cs
+++ Source/Web/GoDrive.Web/ViewModels/Organizations/OrganizationDetailsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace GoDrive.Web.ViewModels.Organizations
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using AutoMapper;
     using Data.Models;
@@ -20,10 +21,13 @@
 
         public string OrganizationImageUrl { get; set; }
 
+        public ICollection<string> CarImages { get; set; }
+
         public void CreateMappings(IMapperConfiguration configuration)
         {
             configuration.CreateMap<Organization, OrganizationDetailsViewModel>()
-               .ForMember(x => x.UsersCount, opt => opt.MapFrom(x => x.Students.Where(c => c.IsDeleted == false).Count()));
+               .ForMember(x => x.UsersCount, opt => opt.MapFrom(x => x.Students.Where(c => c.IsDeleted == false).Count()))
+               .ForMember(x => x.CarImages, opt => opt.MapFrom(x => x.CarsImages.Select(c => c.Url)));
         }
     }
 }
